70e67d6323c3dc7ed18c808a8874f48ca3a236e6
hagmal <mahag2193@oslomet.no>
2024-02-13 11:23:47 +0100

    Lagt til en dropdown-liste og forsøkt å begynne på arrayet

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,14 +1,24 @@
-/* Lag en applikasjon som registrerer kjøp av kinobilletter.
+/* Lag en applikasjon som registrerer kjøp av kinobilletter.*/
+function velgFilm(){
+   //hvor skal den skrives ut til??
+   console.log(document.getElementById("film").value);
+}
 
-Etter at knappen "kjøp billett" er trykket skal billetten lagres som et objekt i array.
+/* Etter at knappen "kjøp billett" er trykket skal billetten lagres som et objekt i array.*/
+const filmRegister = [];
+function visFilmRegister(){
+   //????
+}
 
-Arrayet listes ut under "Alle billetter".
+function register(){
+   const film = document.getElementById("film").value;
+}
+/* Arrayet listes ut under "Alle billetter".*/
 
-Etter at det er registrert en bestilling, skal alle inputfelt "blankes" slik at
-ny billett kan registreres.
+/* Etter at det er registrert en bestilling, skal alle inputfelt "blankes" slik at
+ny billett kan registreres.*/
 
-Knappen "Slett alle billettene" skal slette billettene (tømme arrayet).
+/* Knappen "Slett alle billettene" skal slette billettene (tømme arrayet).*/
 
-Det skal også lages en input-validering som sjekker at alle inputfeltene inneholder noe.
-*/
+/* Det skal også lages en input-validering som sjekker at alle inputfeltene inneholder noe.*/
 
