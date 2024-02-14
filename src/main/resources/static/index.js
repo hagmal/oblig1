a5744bc2be0f2ae960d46eafd82d1be356ab0d3e
hagmal <mahag2193@oslomet.no>
2024-02-14 14:46:05 +0100

    Lagt til valideringer

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,40 +1,58 @@
 // oppretter et tomt array for å lagre billettene
 const billetter = [];
 
+
 function registrerBillett() {
+   document.getElementById("feilAntall").innerHTML = "";
+   document.getElementById("feilFornavn").innerHTML = "";
+   document.getElementById("feilEtternavn").innerHTML = "";
+   document.getElementById("feilTelefonnr").innerHTML = "";
+   document.getElementById("feilEpost").innerHTML = "";
+
    const film = document.getElementById("film").value;
    const antall = Number(document.getElementById("antall").value);
-   // + validering
    const fornavn = document.getElementById("fornavn").value;
-   // + validering
    const etternavn = document.getElementById("etternavn").value;
-   // + validering
    const telefonnr = document.getElementById("telefonnr").value;
-   // + validering
    const epost = document.getElementById("epost").value;
-   // + validering
 
-   // Legger billettene inn i et array
-   const enBillett = {
-      film : film,
-      antall : antall,
-      fornavn : fornavn,
-      etternavn : etternavn,
-      telefonnr : telefonnr,
-      epost : epost
-   };
-   billetter.push(enBillett);
-   visBilletter()
+   if (isNaN(antall) || antall <= 0) {
+      document.getElementById("feilAntall").innerHTML = "Må være et tall større enn 0";
+   }
+   if (fornavn.length === 0 || !isNaN(fornavn)) {
+      document.getElementById("feilFornavn").innerHTML = "Må skrive inn et fornavn";
+   }
+   if (etternavn.length === 0 || !isNaN(etternavn)) {
+      document.getElementById("feilEtternavn").innerHTML = "Må skrive inn et etternavn";
+   }
+   if (isNaN(telefonnr) || telefonnr.length !== 8){
+      document.getElementById("feilTelefonnr").innerHTML = "Telefonnummeret må ha 8 siffer";
+   }
+   if (!/\S+@\S+\.\S+/.test(epost)) {
+      document.getElementById("feilEpost").innerHTML = "Ikke gyldig e-postadresse";
+   }
+   else {
+      // Legger billettene inn i et array
+      const enBillett = {
+         film: film,
+         antall: antall,
+         fornavn: fornavn,
+         etternavn: etternavn,
+         telefonnr: telefonnr,
+         epost: epost
+      };
+      billetter.push(enBillett);
+      visBilletter()
 
-   // Nullstiller input-feltene
-   document.getElementById("film").value="";
-   document.getElementById("antall").value="";
-   document.getElementById("fornavn").value="";
-   document.getElementById("etternavn").value="";
-   document.getElementById("telefonnr").value="";
-   document.getElementById("epost").value="";
+      // Nullstiller input-feltene
+      document.getElementById("film").value = "";
+      document.getElementById("antall").value = "";
+      document.getElementById("fornavn").value = "";
+      document.getElementById("etternavn").value = "";
+      document.getElementById("telefonnr").value = "";
+      document.getElementById("epost").value = "";
+   }
 }
-
 // Hvordan arrayet listes ut under "Alle billetter"
 function visBilletter(){
    let ut = "<table><tr>" +
@@ -45,7 +63,7 @@ function visBilletter(){
       ut += "<tr>";
       ut += "<td>" + b.film + "</td><td>" + b.antall
           + "</td><td>" + b.fornavn + "</td><td>" + b.etternavn + "</td><td>"
-          + b.telefonnr + "</td><td>" + b.epost +"</td>";
+          + b.telefonnr + "</td><td>" + b.epost +"</td></tr>";
    }
    document.getElementById("billetter").innerHTML = ut;
 }
